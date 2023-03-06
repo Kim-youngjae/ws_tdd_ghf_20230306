610662fec26fabc2c57046f60f6f863c98745693
youngjae459 <youngjae459@gmail.com>
2023-03-06 20:13:04 +0900

    명언등록 시 명언번호 증가

--- src/test/java/com/ll/AppTests.java
+++ src/test/java/com/ll/AppTests.java
@@ -58,5 +58,39 @@ public class AppTests {
                 .contains("올바르지 않은 명령입니다.");
     }
 
+    // 2, 3단계
+    @Test
+    @DisplayName("등록화면에서 명언과 작가를 입력받고, 등록시 생성된 명언번호 노출")
+    public void t5() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
 
+        assertThat(rs)
+                .contains("명언 : ")
+                .contains("작가 : ")
+                .contains("1번 명언이 등록되었습니다.")
+                .contains("프로그램이 종료되었습니다.");
+    }
+
+    // 3단계
+    @Test
+    @DisplayName("등록할때 마다 생성되는 명언번호가 1씩 증가")
+    public void t6() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                나의 죽음을 적에게 알리지 마라.
+                이순신
+                """);
+
+        assertThat(rs)
+                .contains("1번 명언이 등록되었습니다.")
+                .contains("2번 명언이 등록되었습니다.")
+                .doesNotContain("3번 명언이 등록되었습니다.");
+    }
 }
