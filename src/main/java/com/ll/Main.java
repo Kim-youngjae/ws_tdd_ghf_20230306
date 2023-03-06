836695271049c04692633614a05de257fa14ac21
youngjae459 <youngjae459@gmail.com>
2023-03-06 15:27:13 +0900

    잘못된 명령어 처리 테스트케이스 추가

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        new App(new Scanner(System.in)).run();
+        new App(new Scanner(System.in)).run(); // main()에서 명언앱 호출
     }
 }
