46cab2f7b09bce7a82c1641550adb2ad99f6efff
youngjae459 <youngjae459@gmail.com>
2023-03-06 13:35:35 +0900

    setting

---
+++ src/test/java/com/ll/AppTests.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTests {
+    @Test
+    void t1() {
+
+    }
+}
